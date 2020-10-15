bf03e8f706d509a6fdaf5f24330a2db137a13b30
Milen Georgiev <menkatageorgiev@abv.bg>
2020-10-15 10:26:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBuSender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "";
+        const string QueueName = "queue1";
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            await SendMessagesAsync(numberOfMessages);
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (int i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                    Console.WriteLine($"Sending message: {messageBody}");
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+
+
+    }
+}
